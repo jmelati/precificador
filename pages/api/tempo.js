2ba8a54b09eef575bba2873d2e29368d5bffab19
jmelati <jmelati@gmail.com>
2021-07-27 15:18:51 -0300

    adiciona variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,24 @@
-function tempo(request, response){
-    const dynamicDate = new Date();
-    
-    response.json({
-        date: dynamicDate.toGMTString()
-    })
-}
+const axios = require('axios');
 
-export default tempo;
+async function PegarTabelaCadastro(Telefone) {
+    const apiSecret = process.env.SHEETDB;
+    return axios.get(`https://sheet.best/api/sheets/${apiSecret}/search?Telefone=${Telefone}`);  
+  }
+
+async function PegarStatus(Telefone, response){
+    const dynamicDate = new Date();
+    let status = 'não existe';
+    Telefone = "554799549637";
+    return PegarTabelaCadastro(Telefone).then(res => {
+      res.data.map(person => {
+        status = person.Status;
+      });
+    console.log(status);
+    response.json({
+        Date: dynamicDate.toGMTString(),
+        Status: status
+    })
+    });
+  }
+
+export default PegarStatus;
